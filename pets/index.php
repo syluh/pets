574f89f91470d22acf8a93b4124fc3e4d8ccfdb4
vecarrion <73541103+syluh@users.noreply.github.com>
2022-11-01 16:49:57 -0500

    Add files via upload

---
+++ pets/index.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Happy Pets</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="icon" href="images/logo.png" sizes="32x32" />
+    <link rel="icon" href="images/logo.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="images/logo.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+</head>
+<body>
+<div class="contenedor">
+	<header class="cabeceraPrincipal">
+		<section class="logotipo"><img src="images/logotipoPet.png"></section>
+		<nav class="menuPrincipal">
+			<a href="#">Inicio</a>
+			<a href="#">Servicios</a>
+			<a href="#">Productos</a>
+			<a href="#">Adopción</a>
+			<a href="#">Contactos</a>
+		</nav>
+	</header>
+	<section class="sliderPrincipal">
+		<img src="images/dogs.jpg">
+	</section>
+	<section class="titulo">
+		<h2>Adopcion</h2>
+	</section>
+	<section class="adopcion1">
+		<img src="images/dog1.jpg">
+		<section class="cardog">
+			<h3>firulais</h3>
+			<p>El perro,llamado perro doméstico o can, y en algunos lugares coloquialmente llamado chucho,  tuso,  choco,  entre otros; es un mamífero carnívoro de la familia de los cánidos, que constituye una especie del género Canis.
+			</p>
+		</section>
+	</section>
+	<section class="adopcion2"><h3>Noticia 2</h3></section>
+	<section class="doctor">
+		<h3>Doctores</h3>
+	</section>
+	<section class="sponsor">
+		<h3>Sponsor</h3>
+	</section>
+	<footer class="piePagina">
+		<section class="derechos">
+			<h6>Derechos Reservados UTPL 2022</h6>
+		</section>
+		<nav class="redes">
+			<a href=""><i class="fa-brands fa-facebook"></i></a>
+			<a href=""><i class="fa-brands fa-instagram"></i></a>
+			<a href=""><i class="fa-brands fa-tiktok"></i></a>
+		</nav>
+	</footer>
+</div>
+</body>
+</html>
