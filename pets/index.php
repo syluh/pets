1a6766eaf716d5247d43bb17dc355421f885db36
Syluh <vecarrion3@utpl.edu.ec>
2022-11-08 11:03:09 -0500

    se añaden doctores y un gato

--- pets/index.php
+++ pets/index.php
@@ -38,9 +38,48 @@
 			</p>
 		</section>
 	</section>
-	<section class="adopcion2"><h3>Noticia 2</h3></section>
-	<section class="doctor">
-		<h3>Doctores</h3>
+	<section class="adopcion2">
+		<img src="images/cat.jpg">
+		<section class="carcat">
+			<h3>Michi</h3>
+			<p>
+				El gato doméstico, llamado más comúnmente gato, y de forma coloquial minino,  michino,  michi,  micho,  mizo,  miz, o morrongo,  y algunos nombres más, es un mamífero carnívoro de la familia Felidae. 
+			</p>
+		</section>
+	</section>
+
+	<section class="titulo1">
+		<h2>Doctores</h2>
+	</section>
+
+	<section class="doctores">
+		<section class="doctor1">
+		<img src="images/doctor1.jpg">
+		<section class="doc1">
+			<h3>Jhonny Sins</h3>
+			<p>
+				Quizás el médico más famoso en Medicina es quizás poco conocido para el gran público, pero a Sir William Osler se le considera el padre de la práctica clínica moderna. 
+			</p>
+		</section>
+	</section>
+	<section class="doctor2">
+		<img src="images/doctor2.jpg">
+		<section class="doc2">
+			<h3>Hipocrates</h3>
+			<p>
+				Nacido en el 460 a.C. a Hipócrates se le llama el padre de la medicina occidental. Suyo es el juramento hipocrático que a día de hoy sigue siendo el referente ético de la profesión.  Aparte de ello.
+			</p>
+		</section>
+	</section>
+	<section class="doctor3">
+		<img src="images/doctor3.jpg">
+		<section class="doc3">
+			<h3>Ignaz Semmelweis</h3>
+			<p>
+				El médico húngaro Ignaz Semmelweis se hizo famoso en el siglo XIX por establecer unas nuevas condiciones sanitarias en el quirófano y en el entorno sanitario. 
+			</p>
+		</section>
+	</section>
 	</section>
 	<section class="sponsor">
 		<h3>Sponsor</h3>
